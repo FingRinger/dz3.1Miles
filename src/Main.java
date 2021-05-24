843fb6427f93141b653b779d0601aa7c19d197fd
FingRinger <ard21study@gmail.com>
2021-05-24 15:17:33 +0300

    ticket Price 10 000

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+long ticketPrice = 10_000;
+int amountOfRublesForOneMile = 20;
+long miles = ticketPrice / amountOfRublesForOneMile;
+        System.out.println(miles);
+
+    }
+}
